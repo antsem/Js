6fe666ac2b77f962dbc37cbbac01b80cf5bb2428
antsem <commerce-design@yandex.ru>
2016-05-25 11:17:50 +0600

    Events

--- JavaScript/Events.js
+++ JavaScript/Events.js
@@ -1,3 +1,35 @@
 /**
  * Events
  */
+
+/**
+ * Add event
+ */
+
+element.addEventListener(event, handler);
+
+/**
+ * Remove event
+ */
+
+element.removeEventListener(event, handler);
+
+/**
+ * Событие загрузки документа
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    // code...
+});
+
+/**
+ * IE8- add event
+ */
+
+element.attachEvent("on" + event, handler);
+
+/**
+ * IE8- remove event
+ */
+
+element.detachEvent("on" + event, handler);
